c50afc513bc0e9d281d126b4946551174a1ae091
BaiBeiCha <myvvot@gmail.com>
2026-02-02 23:48:44 +0300

    Created Dockerfiles and Nexus Sync Project

---
+++ Nexus Sync Service/src/test/java/by/nexus/sync/NexusSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nexus.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusSyncServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
